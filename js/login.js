8d4e74b4b257b5bacbed6eca91a6be08e38ed642
Ignacio1224 <igaciocabrera1224@gmail.com>
2018-05-25 16:29:37 -0300

    Hide textbox and labels

--- js/login.js
+++ js/login.js
@@ -2,4 +2,22 @@ $(document).ready(initLogin);
 
 function initLogin() {
     $('#loginBox').fadeIn('slow');
+    hideFields();
+}
+
+function hideFields() {
+    if (document.getElementById("option1").checked === true) {
+        document.getElementById("lblDocument").style.display = "none";
+        document.getElementById("txtDocument").style.display = "none";
+    } else {
+        document.getElementById("lblDocument").style.display = "block";        
+        document.getElementById("txtDocument").style.display = "block";
+    }
+    if (document.getElementById("option2").checked === true) {
+        document.getElementById("lblNumber").style.display = "none";        
+        document.getElementById("txtNumber").style.display = "none";
+    } else {
+        document.getElementById("lblNumber").style.display = "block";
+        document.getElementById("txtNumber").style.display = "block";        
+    }
 }
