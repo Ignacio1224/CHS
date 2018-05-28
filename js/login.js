1aa98d222cc4fc83c7b67078f54ec959ff94be6a
Matias Schmid <playmobilman@gmail.com>
2018-05-28 02:36:56 -0300

    Cuadro de login, cambio de radios por select en perfiles de acceso

--- js/login.js
+++ js/login.js
@@ -1,23 +1,34 @@
+// hook up events
 $(document).ready(initLogin);
+$('#slcProfile').change(updateAccessFields);
 
 function initLogin() {
     $('#loginBox').fadeIn('slow');
-    hideFields();
+    updateAccessFields();
 }
 
-function hideFields() {
-    if (document.getElementById("option1").checked === true) {
-        document.getElementById("lblDocument").style.display = "none";
-        document.getElementById("txtDocument").style.display = "none";
+function updateAccessFields() {
+    if ($('#slcProfile').val() == 'M') {
+        $("#ctlGroupDocument").hide();
+        $("#ctlGroupNumber").show();
     } else {
-        document.getElementById("lblDocument").style.display = "block";        
-        document.getElementById("txtDocument").style.display = "block";
-    }
-    if (document.getElementById("option2").checked === true) {
-        document.getElementById("lblNumber").style.display = "none";        
-        document.getElementById("txtNumber").style.display = "none";
-    } else {
-        document.getElementById("lblNumber").style.display = "block";
-        document.getElementById("txtNumber").style.display = "block";        
+        $("#ctlGroupNumber").hide();
+        $("#ctlGroupDocument").show();
     }
 }
+
+// function hideFields() {
+//     if (document.getElementById("option1").checked === true) {
+        
+//     } else {
+//         document.getElementById("lblDocument").style.display = "block";        
+//         document.getElementById("txtDocument").style.display = "block";
+//     }
+//     if (document.getElementById("option2").checked === true) {
+//         document.getElementById("lblNumber").style.display = "none";        
+//         document.getElementById("txtNumber").style.display = "none";
+//     } else {
+//         document.getElementById("lblNumber").style.display = "block";
+//         document.getElementById("txtNumber").style.display = "block";        
+//     }
+// }
