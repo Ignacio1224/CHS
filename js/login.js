fe3fade7060b635d66e80d62cbf7777e635f9216
Ignacio1224 <igaciocabrera1224@gmail.com>
2018-05-28 17:04:17 -0300

    deleted some trash

--- js/login.js
+++ js/login.js
@@ -16,19 +16,3 @@ function updateAccessFields() {
         $("#ctlGroupDocument").show();
     }
 }
-
-// function hideFields() {
-//     if (document.getElementById("option1").checked === true) {
-        
-//     } else {
-//         document.getElementById("lblDocument").style.display = "block";        
-//         document.getElementById("txtDocument").style.display = "block";
-//     }
-//     if (document.getElementById("option2").checked === true) {
-//         document.getElementById("lblNumber").style.display = "none";        
-//         document.getElementById("txtNumber").style.display = "none";
-//     } else {
-//         document.getElementById("lblNumber").style.display = "block";
-//         document.getElementById("txtNumber").style.display = "block";        
-//     }
-// }
