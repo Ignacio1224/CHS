3611c77785b2ab8b97729ac11c83cbdd355e182a
Ignacio1224 <ignaciocabrera1224@gmail.com>
2018-05-31 11:02:32 -0300

    some getters of doctor

--- js/db.js
+++ js/db.js
@@ -1,14 +1,41 @@
 /** Doctor Functions */
 
-/**Getters */
+/** Getters */
 
+
+/** Get Name, input his dNumber */
 function getDoctorName(_dNumber) {
     let name = "";
-    for (let i = 0; i < doctors.length; i++) {
+    for (let i = 0, l = doctors.length; i < l; i++) {
         if (doctors[i].dNumber === _dNumber) {
             name = doctors[i].name;
             break;
         }
     }
     return name;
+}
+
+/** Get Speciality, imput his dNumber */
+function getSpeciality(_dNumber) {
+    let speciality = "";
+    for (let i = 0, l = doctors.length; i < l; i++) {
+        if (doctors[i].dNumber === _dNumber) {
+            speciality = doctors[i].speciality;
+            break;
+        }
+    }
+    return speciality;
+}
+
+/** Get an array of dNumbers candidates with the name that was introduced in the function */
+function getDoctorNumber(_name) {
+    let numbers = [];
+    let n = 0;
+    for (let i = 0, l = doctors.length; i < l; i++) {
+        if (doctors[i].name === _name) {
+            numbers[n] = doctors[i].dNumber;
+            n++;
+        }
+    }
+    return numbers;
 }
