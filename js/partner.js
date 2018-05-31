a4d9a907d5ccbba3498156d9414bba06ac033d03
Matias Schmid <playmobilman@gmail.com>
2018-05-31 00:47:31 -0300

    Inicio de panel de socio, construcción del dashboard base

--- js/partner.js
+++ js/partner.js
@@ -1,4 +1,12 @@
+
+$('#partner-tab a').on('click', function(e) {
+    console.log(e.target.text);
+    $('.breadcrumb-item.active').first().text(e.target.text);
+});
+
+
 function closeSession() {
     window.close();
     location.reload();
 }
+
