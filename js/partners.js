b8939f1fa48dbc2ebe20dc9db80e0117ea84869c
Ignacio1224 <ignaciocabrera1224@gmail.com>
2018-05-31 22:17:01 -0300

    added: some functions to db, toDoList,  stories and + partners & doctors

--- js/partners.js
+++ js/partners.js
@@ -4,6 +4,36 @@ let partners = [
         "name": "Ignacio Cabrera",
         "medicalHeader": "General Medicine",
         "password": "//1234"
+    },
+    {
+        "document": 4927439,
+        "name": "Pepe Griyo",
+        "medicalHeader": "General Medicine",
+        "password": "//1234"
+    },
+    {
+        "document": 492743,
+        "name": "Constancio Torremorada",
+        "medicalHeader": "General Medicine",
+        "password": "//1234"
+    },
+    {
+        "document": 49274398,
+        "name": "Emila Nessa",
+        "medicalHeader": "General Medicine",
+        "password": "//1234"
+    },
+    {
+        "document": 49274624,
+        "name": "Roberto Carlos",
+        "medicalHeader": "General Medicine",
+        "password": "//1234"
+    },
+    {
+        "document": 49271561,
+        "name": "Armando Castillos",
+        "medicalHeader": "General Medicine",
+        "password": "//1234"
     }
 
 ];
---
+++ js/stories.js
@@ -0,0 +1,13 @@
+let stories = [
+    {
+        "nStorie": 1,
+        "partnerDocument": 7896541,
+        "doctorNumber": 123456,
+        "date": "2018 - 02 - 20", // YYYY - MM - DD
+        "reason": "Texto del motivo de la consulta…",
+        "diagnostic": "Texto del diagnóstico del médico….",
+        "prescription": "texto de la prescripción del médico...",
+        "picture": "archivo.jpg"
+    }
+    
+];
